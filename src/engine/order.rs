a88f3f3b874bd211bfebc4b43c523be4c0e1e2a3
adecess <axel.saliniere@protonmail.com>
2026-04-12 20:24:16 +0200

    Update order subtypes and add a constructor function

--- src/engine/order.rs
+++ src/engine/order.rs
@@ -1,15 +1,33 @@
 #[derive(Debug, Copy, Clone)]
-pub struct Price(i64);
+pub struct Price(u64);
 #[derive(Debug, Copy, Clone)]
-pub struct Qty(i64);
+pub struct Qty(u64);
 #[derive(Debug, Copy, Clone)]
 pub enum Side {
     Buy,
     Sell,
 }
 
+#[derive(Debug, Clone)]
 pub struct Order {
+    order_id: u64,
     price: Price,
     quantity: Qty,
     side: Side,
+}
+
+impl Order {
+    pub fn new(
+        order_id: u64,
+        price: Price,
+        quantity: Qty,
+        side: Side,
+    ) -> Order {
+        Order {
+            order_id,
+            quantity,
+            price,
+            side,
+        }
+    }
 }
