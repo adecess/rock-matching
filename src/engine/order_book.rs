d21b9d1519ab48c6ce390a62a54439c1f958a88f
adecess <axel.saliniere@protonmail.com>
2026-04-25 17:59:31 +0200

    Complete incoming buy trade scenarios

--- src/engine/order_book.rs
+++ src/engine/order_book.rs
@@ -43,53 +43,58 @@ impl OrderBook {
     fn match_limit_order(&mut self, mut incoming_order: Order) -> Vec<Event> {
         let mut events = Vec::new();
 
-        let order_id = incoming_order.order_id;
-        let order_price = incoming_order.price;
-        let remaining_order_quantity = incoming_order.quantity;
-        let order_side = incoming_order.side;
-
-        match order_side {
+        match incoming_order.side {
             Side::Buy => {
-                while let Some(mut ask_price_level) = self.sell_orders.first_entry() {
-                    match ask_price_level.key().cmp(&order_price) {
+                while incoming_order.quantity > Qty(0)
+                    && let Some(mut ask_price_level) = self.sell_orders.first_entry()
+                {
+                    match ask_price_level.key().cmp(&incoming_order.price) {
                         Ordering::Greater => {
-                            self.add_to_book(order_price, Side::Buy, incoming_order);
-                            events.push(Event::OrderAddedToBook(
-                                order_id,
-                                Side::Buy,
-                                order_price,
-                                remaining_order_quantity,
-                            ));
                             break;
                         }
                         _ => {
                             let resting_order = ask_price_level.get_mut().front_mut().unwrap();
-
-                            let trade_size = min(remaining_order_quantity, resting_order.quantity);
+                            let trade_size = min(incoming_order.quantity, resting_order.quantity);
 
                             events.push(Event::OrderTraded {
-                                taker: order_id,
+                                taker: incoming_order.order_id,
                                 maker: resting_order.order_id,
                                 taker_side: Side::Buy,
-                                price: order_price,
+                                price: resting_order.price,
                                 quantity: trade_size,
                             });
 
                             resting_order.quantity = resting_order.quantity - trade_size;
-                            incoming_order.quantity = remaining_order_quantity - trade_size;
+                            incoming_order.quantity = incoming_order.quantity - trade_size;
 
-                            if resting_order.quantity.0 == 0 {
+                            if resting_order.quantity == Qty(0) {
                                 ask_price_level.get_mut().pop_front();
-                            }
 
-                            if ask_price_level.get_mut().is_empty() {
-                                ask_price_level.remove();
+                                if ask_price_level.get_mut().is_empty() {
+                                    ask_price_level.remove();
+                                }
                             }
                         }
                     }
                 }
+
+                let order_id = incoming_order.order_id;
+                let order_price = incoming_order.price;
+                let remaining_order_quantity = incoming_order.quantity;
+                if remaining_order_quantity > Qty(0) {
+                    self.add_to_book(incoming_order.price, Side::Buy, incoming_order);
+                    events.push(Event::OrderAddedToBook(
+                        order_id,
+                        Side::Buy,
+                        order_price,
+                        remaining_order_quantity,
+                    ));
+                }
             }
             Side::Sell => {
+                let order_id = incoming_order.order_id;
+                let order_price = incoming_order.price;
+                let remaining_order_quantity = incoming_order.quantity;
                 if let Some(bid_price_level) = self.buy_orders.last_entry() {
                     match bid_price_level.key().cmp(&order_price) {
                         Ordering::Less => {
@@ -125,18 +130,177 @@ mod tests {
     use crate::engine::order::{OrderId, Qty};
 
     #[test]
-    fn limit_order_is_added_to_order_book_if_there_is_no_matching_order() {
+    fn limit_sell_order_is_added_to_order_book_if_there_is_no_matching_order() {
         let mut order_book = OrderBook::default();
         let order_id = OrderId(0);
         let price = Price(100);
         let order = Order::new(order_id, price, Qty(5), Side::Sell);
 
-        let event = order_book.match_limit_order(order);
+        let events = order_book.match_limit_order(order);
         assert_eq!(
-            event[0],
-            Event::OrderAddedToBook(order_id, Side::Sell, price, Qty(5))
+            events.first(),
+            Some(&Event::OrderAddedToBook(
+                order_id,
+                Side::Sell,
+                price,
+                Qty(5)
+            ))
         );
         assert_eq!(order_book.sell_orders.len(), 1);
         assert_eq!(order_book.buy_orders.len(), 0);
+    }
+
+    #[test]
+    fn limit_buy_order_trades_if_there_is_a_perfectly_matching_sell_order() {
+        let mut order_book = OrderBook::default();
+        order_book.sell_orders.insert(
+            Price(99),
+            VecDeque::from([Order::new(OrderId(0), Price(99), Qty(5), Side::Sell)]),
+        );
+
+        let incoming_buy_order = Order::new(OrderId(1), Price(100), Qty(5), Side::Buy);
+
+        let events = order_book.match_limit_order(incoming_buy_order);
+        assert_eq!(
+            events[0],
+            Event::OrderTraded {
+                taker: OrderId(1),
+                maker: OrderId(0),
+                taker_side: Side::Buy,
+                price: Price(99),
+                quantity: Qty(5)
+            }
+        );
+        assert_eq!(order_book.sell_orders.len(), 0);
+        assert_eq!(order_book.buy_orders.len(), 0);
+    }
+
+    #[test]
+    fn limit_buy_order_trades_if_there_are_enough_matching_sell_orders_at_the_same_price_level() {
+        let mut order_book = OrderBook::default();
+        order_book.sell_orders.insert(
+            Price(99),
+            VecDeque::from([
+                Order::new(OrderId(0), Price(99), Qty(3), Side::Sell),
+                Order::new(OrderId(1), Price(99), Qty(2), Side::Sell),
+            ]),
+        );
+
+        let incoming_buy_order = Order::new(OrderId(2), Price(100), Qty(5), Side::Buy);
+
+        let events = order_book.match_limit_order(incoming_buy_order);
+        assert_eq!(
+            events,
+            [
+                Event::OrderTraded {
+                    taker: OrderId(2),
+                    maker: OrderId(0),
+                    taker_side: Side::Buy,
+                    price: Price(99),
+                    quantity: Qty(3)
+                },
+                Event::OrderTraded {
+                    taker: OrderId(2),
+                    maker: OrderId(1),
+                    taker_side: Side::Buy,
+                    price: Price(99),
+                    quantity: Qty(2)
+                }
+            ]
+        );
+        assert_eq!(order_book.sell_orders.len(), 0);
+        assert_eq!(order_book.buy_orders.len(), 0);
+    }
+
+    #[test]
+    fn limit_buy_order_trades_if_there_are_enough_matching_sell_orders_at_different_price_levels() {
+        let mut order_book = OrderBook::default();
+        order_book.sell_orders.insert(
+            Price(99),
+            VecDeque::from([
+                Order::new(OrderId(0), Price(99), Qty(1), Side::Sell),
+                Order::new(OrderId(1), Price(99), Qty(2), Side::Sell),
+                Order::new(OrderId(2), Price(100), Qty(2), Side::Sell),
+            ]),
+        );
+
+        let incoming_buy_order = Order::new(OrderId(3), Price(100), Qty(5), Side::Buy);
+
+        let events = order_book.match_limit_order(incoming_buy_order);
+        assert_eq!(
+            events,
+            [
+                Event::OrderTraded {
+                    taker: OrderId(3),
+                    maker: OrderId(0),
+                    taker_side: Side::Buy,
+                    price: Price(99),
+                    quantity: Qty(1)
+                },
+                Event::OrderTraded {
+                    taker: OrderId(3),
+                    maker: OrderId(1),
+                    taker_side: Side::Buy,
+                    price: Price(99),
+                    quantity: Qty(2)
+                },
+                Event::OrderTraded {
+                    taker: OrderId(3),
+                    maker: OrderId(2),
+                    taker_side: Side::Buy,
+                    price: Price(100),
+                    quantity: Qty(2)
+                }
+            ]
+        );
+        assert_eq!(order_book.sell_orders.len(), 0);
+        assert_eq!(order_book.buy_orders.len(), 0);
+    }
+
+    #[test]
+    fn limit_buy_order_trades_partially_if_there_are_not_enough_matching_sell_orders_at_different_price_levels()
+     {
+        let mut order_book = OrderBook::default();
+        order_book.sell_orders.insert(
+            Price(99),
+            VecDeque::from([
+                Order::new(OrderId(0), Price(99), Qty(1), Side::Sell),
+                Order::new(OrderId(1), Price(99), Qty(1), Side::Sell),
+                Order::new(OrderId(2), Price(100), Qty(1), Side::Sell),
+            ]),
+        );
+
+        let incoming_buy_order = Order::new(OrderId(3), Price(100), Qty(5), Side::Buy);
+
+        let events = order_book.match_limit_order(incoming_buy_order);
+        assert_eq!(
+            events,
+            [
+                Event::OrderTraded {
+                    taker: OrderId(3),
+                    maker: OrderId(0),
+                    taker_side: Side::Buy,
+                    price: Price(99),
+                    quantity: Qty(1)
+                },
+                Event::OrderTraded {
+                    taker: OrderId(3),
+                    maker: OrderId(1),
+                    taker_side: Side::Buy,
+                    price: Price(99),
+                    quantity: Qty(1)
+                },
+                Event::OrderTraded {
+                    taker: OrderId(3),
+                    maker: OrderId(2),
+                    taker_side: Side::Buy,
+                    price: Price(100),
+                    quantity: Qty(1)
+                },
+                Event::OrderAddedToBook(OrderId(3), Side::Buy, Price(100), Qty(2),)
+            ]
+        );
+        assert_eq!(order_book.sell_orders.len(), 0);
+        assert_eq!(order_book.buy_orders.len(), 1);
     }
 }
