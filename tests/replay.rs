c1ac056f98a9fbabf446f0ec0d317343de545d68
adecess <axel.saliniere@protonmail.com>
2026-05-24 11:50:34 +0200

    tech(replay): make determinism test more exhaustive

--- tests/replay.rs
+++ tests/replay.rs
@@ -1,11 +1,11 @@
-use rock_matching::engine::core::Command::SubmitOrder;
+use rock_matching::engine::core::Command::{CancelOrder, SubmitOrder};
 use rock_matching::engine::core::OrderType::Limit;
 use rock_matching::engine::core::{ApplyError, Engine, Timestamp};
-use rock_matching::engine::order::{Price, Qty, Side};
+use rock_matching::engine::order::{OrderId, Price, Qty, Side};
 use rock_matching::engine::order_book::Event;
 
 #[test]
-fn two_engines_with_the_same_submit_order_commands_produce_the_same_events() {
+fn two_engines_with_the_same_commands_produce_the_same_events() {
     let mut first_engine = Engine::default();
     let mut first_events: Vec<Result<Vec<Event>, ApplyError>> = Vec::new();
 
@@ -18,7 +18,7 @@ fn two_engines_with_the_same_submit_order_commands_produce_the_same_events() {
     first_events.push(first_engine.apply(SubmitOrder {
         timestamp: Timestamp(2),
         quantity: Qty(3),
-        side: Side::Buy,
+        side: Side::Sell,
         order_type: Limit(Price(100)),
     }));
     first_events.push(first_engine.apply(SubmitOrder {
@@ -26,6 +26,10 @@ fn two_engines_with_the_same_submit_order_commands_produce_the_same_events() {
         quantity: Qty(1),
         side: Side::Buy,
         order_type: Limit(Price(100)),
+    }));
+    first_events.push(first_engine.apply(CancelOrder {
+        timestamp: Timestamp(4),
+        order_id: OrderId(1),
     }));
 
     let mut second_engine = Engine::default();
@@ -40,7 +44,7 @@ fn two_engines_with_the_same_submit_order_commands_produce_the_same_events() {
     second_events.push(second_engine.apply(SubmitOrder {
         timestamp: Timestamp(2),
         quantity: Qty(3),
-        side: Side::Buy,
+        side: Side::Sell,
         order_type: Limit(Price(100)),
     }));
     second_events.push(second_engine.apply(SubmitOrder {
@@ -49,7 +53,11 @@ fn two_engines_with_the_same_submit_order_commands_produce_the_same_events() {
         side: Side::Buy,
         order_type: Limit(Price(100)),
     }));
-
+    second_events.push(second_engine.apply(CancelOrder {
+        timestamp: Timestamp(4),
+        order_id: OrderId(1),
+    }));
+    
     assert_eq!(first_engine, second_engine);
     assert_eq!(first_events, second_events);
 }
