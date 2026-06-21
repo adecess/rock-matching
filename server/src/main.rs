869902ee4f345b2e952a84b219e8b80073e937f4
adecess <axel.saliniere@protonmail.com>
2026-06-21 18:05:34 +0200

    feat(server): implement simple working version of the server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,33 @@
-use rock_matching_engine::Engine;
+use tokio::sync::mpsc::channel;
+use rock_matching_engine::{Command, Engine, Price, Qty, Side, Timestamp};
+use rock_matching_engine::Command::SubmitOrder;
+use rock_matching_engine::OrderType::Limit;
 
-fn main() {
-    let engine = Engine::default();
-    println!("{engine:?}");
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = channel::<Command>(100);
+    let mut engine = Engine::default();
+    let handle = tokio::spawn(async move {
+        while let Some(command) = rx.recv().await {
+            let events = engine.apply(command).unwrap();
+            println!("{:?}", events);
+        }
+    });
+
+    tx.send(SubmitOrder {
+        timestamp: Timestamp(1),
+        quantity: Qty(1),
+        side: Side::Buy,
+        order_type: Limit(Price(102)),
+    }).await.unwrap();
+
+    tx.send(SubmitOrder {
+        timestamp: Timestamp(2),
+        quantity: Qty(1),
+        side: Side::Sell,
+        order_type: Limit(Price(101)),
+    }).await.unwrap();
+
+    drop(tx);
+    handle.await.unwrap();
 }
