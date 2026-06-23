b126ea62ca6689454881a68b5a8d278fe0d5e001
adecess <axel.saliniere@protonmail.com>
2026-06-23 22:05:45 +0200

    feat(server): extract engine task logic

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,9 @@ use rock_matching_engine::Command::SubmitOrder;
 use rock_matching_engine::OrderType::Limit;
 use rock_matching_engine::{BookSnapshot, Command, Engine, Event, Price, Qty, Side, Timestamp};
 use tokio::sync::broadcast;
+use tokio::sync::broadcast::Sender;
 use tokio::sync::mpsc;
+use tokio::sync::mpsc::Receiver;
 
 #[derive(Debug, Clone)]
 struct ServerEvent {
@@ -12,7 +14,7 @@ struct ServerEvent {
 
 #[tokio::main]
 async fn main() {
-    let mut engine = Engine::default();
+    let engine = Engine::default();
 
     let (broadcast_tx, mut broadcast_rx) = broadcast::channel::<ServerEvent>(16);
     let listener_handle = tokio::spawn(async move {
@@ -24,33 +26,19 @@ async fn main() {
         }
     });
 
-    let (tx, mut rx) = mpsc::channel::<Command>(100);
+    let (tx, rx) = mpsc::channel::<Command>(100);
     let engine_handle = tokio::spawn(async move {
-        while let Some(command) = rx.recv().await {
-            let events = engine.apply(command).unwrap();
-            let mut last_price = None;
-            for event in events {
-                if let Event::OrderTraded { price, .. } = event {
-                    last_price = Some(price);
-                }
-            }
-
-            let server_event = ServerEvent {
-                snapshot: engine.top_levels(10),
-                last_price,
-            };
-            let _ = broadcast_tx.send(server_event);
-        }
+        run_engine_task(rx, broadcast_tx, engine).await;
     });
 
     tx.send(SubmitOrder {
         timestamp: Timestamp(1),
-        quantity: Qty(1),
+        quantity: Qty(4),
         side: Side::Buy,
         order_type: Limit(Price(102)),
     })
-    .await
-    .unwrap();
+        .await
+        .unwrap();
 
     tx.send(SubmitOrder {
         timestamp: Timestamp(2),
@@ -58,10 +46,48 @@ async fn main() {
         side: Side::Sell,
         order_type: Limit(Price(101)),
     })
-    .await
-    .unwrap();
+        .await
+        .unwrap();
+
+    tx.send(SubmitOrder {
+        timestamp: Timestamp(0),
+        quantity: Qty(1),
+        side: Side::Buy,
+        order_type: Limit(Price(100)),
+    })
+        .await
+        .unwrap();
 
     drop(tx);
     engine_handle.await.unwrap();
     listener_handle.await.unwrap();
+}
+
+async fn run_engine_task(
+    mut mpsc_receiver: Receiver<Command>,
+    broadcast_sender: Sender<ServerEvent>,
+    mut engine: Engine,
+) {
+    let mut last_price = None;
+
+    while let Some(command) = mpsc_receiver.recv().await {
+        match engine.apply(command) {
+            Ok(events) => {
+                for event in events {
+                    if let Event::OrderTraded { price, .. } = event {
+                        last_price = Some(price);
+                    }
+                }
+
+                let server_event = ServerEvent {
+                    snapshot: engine.top_levels(10),
+                    last_price,
+                };
+                let _ = broadcast_sender.send(server_event);
+            }
+            Err(error) => {
+                eprintln!("failed to apply command: {error:?}");
+            }
+        }
+    }
 }
